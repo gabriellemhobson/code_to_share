65d2ffc44df036e64ba999a76ebfa7019ba9e952
gabriellemhobson <ghobson@ucsd.edu>
2020-10-19 13:59:22 -0400

    Update reproducing_TandS_fig441.py

--- reproducing_TandS_fig441.py
+++ reproducing_TandS_fig441.py
@@ -1,32 +1,43 @@
-# A script to reproduce figure 4.41 from Geodynamics: Third Edition, Turcotte and Schubert, 2014 
+'''
+A script to reproduce figure 4.41 from Geodynamics: Third Edition, Turcotte and Schubert, 2014 
 
-# Figure 4.41 shows temperature profiles at the center of a 2 m wide intrusion at specific times
+Previously solved for the temperature distribution up until solidification of the dike. 
+Now, solving 1D unsteady heat conduction problem in an infinite region with specified initial temperature
+distribution, to get subsequent thermal history. 
+
+The heat conduction equation is nondimensionalized and solved to get the temperature distribution T-T0,
+and this temperature distribution is plotted at times t = 100 days and t = 1000 days in figure 4.41. 
+The intrusive body under consideration in this example is a 2 m wide intrusion, and the temperature 
+profiles at the center of the intrusion are plotted. That intrusion had a solidification time of 10.9 days, 
+so our timescale must be much longer than that. 
+'''
+
+# importing required libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 animate = 0    # set animate=1 if you want to plot the solution over time and save frames as jpg to make a video
 
-n = 256
+n = 256 # the number of points on the 1D grid
 y = np.linspace(0,20,n) # y is the horizontal axis, in m
-Q = 8.8e9 # J m^-2
+Q = 8.8e9 # J m^-2, initial heat content of the dike
 rho = 2900 # kg m^-3
 c = 1.2 * 1000 # kJ kg^-1 K^-1 converted to J kg^-1 K^-1
 kappa = 0.5 * 1e-6 # mm^2 s^-1 converted to m^2 s^-1
 
-def temp(Q,rho,c,kappa,y,t):
+def temp(Q,rho,c,kappa,y,t): # the function we wish to plot
     T = (Q / (2*rho*c*np.sqrt(np.pi*kappa*t))) * (np.exp(-y**2 /(4*kappa*t)))
     return T
 
-def time(t):
-    # convert time from days to seconds
+def time(t): # convert time from days to seconds
     ts = t*24*60*60;
     return ts
 
 plt.plot(y,temp(Q,rho,c,kappa,y,time(100)))
 plt.plot(y,temp(Q,rho,c,kappa,y,time(1000)))
 
-fs = 14
+fs = 14 # set the font size 
 plt.legend(['t = 100 days','t = 1000 days'])
 plt.xlabel(r'$y (m)$',fontsize=fs)
 plt.ylabel(r'$(T - T_0) (K)$',fontsize=fs)
@@ -42,13 +53,12 @@ plt.show()
 
 if animate == 1:
 
-    fs = 14
-    t = np.arange(50,1001,1)
+    t = np.arange(50,1001,1) # timesteps we want to plot
 
     frame = 0
     for k in range(951):
         frame += 1
-        plt.cla()
+        plt.cla() # clear the plot
 
         plt.title('t = {}'.format(t[k]))
         plt.plot(y,temp(Q,rho,c,kappa,y,time(t[k])))
